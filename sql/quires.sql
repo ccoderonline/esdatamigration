3a0abf2f43fd8578300cf314b9e1af33c3a854f6
ezj1kor <kiran.teja@in.bosch.com>
2025-01-11 22:09:33 +0530

    [SQL] Add and update basic quires to drop tables

--- sql/quires.sql
+++ sql/quires.sql
@@ -11,4 +11,7 @@ DROP SCHEMA SCHEMA_NAME_TO_DELETE;
 -- List available tables in schema
 SELECT table_name
 FROM information_schema.tables
-WHERE table_schema = 'estest';
+WHERE table_schema = 'YOUR_SCHEMA_NAME';
+
+-- Delete table by name
+DROP TABLE YOUR_SCHEMA_NAME.YOUR_TABLE_NAME;
